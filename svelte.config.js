8d1218971a6b5c7ea09f22e7d96ca635f4ea8619
Krisna <krisnaepras@gmail.com>
2026-01-01 01:52:07 +0700

    up

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
+const adapter = process.env.ADAPTER === 'static'
+	? (await import('@sveltejs/adapter-static')).default
+	: (await import('@sveltejs/adapter-vercel')).default;
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,9 +13,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: 'index.html'
-		})
+		adapter: process.env.ADAPTER === 'static'
+			? adapter({
+				fallback: 'index.html'
+			})
+			: adapter()
 	}
 };
 
